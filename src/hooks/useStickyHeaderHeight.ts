bf794080a6fae8e04450ea2fed30ae63f715c148
Gaida777 <gaida777777@gmail.com>
2026-06-06 15:33:46 +0200

    feat(frontend): fix header dynamic height to be set before layout render

--- src/hooks/useStickyHeaderHeight.ts
+++ src/hooks/useStickyHeaderHeight.ts
@@ -2,6 +2,17 @@
 import { getElementSize } from '@helpers/getElementSize';
 import { useEffect } from 'react';
 
+const HEADER_HEIGHT_VAR = '--header-height';
+const HEADER_POSITION_VAR = '--header-position';
+export const setHeaderHeightVariable = (height: number) => {
+    document.documentElement.style.setProperty(
+        HEADER_HEIGHT_VAR,
+        `${Math.round(height)}px`,
+    );
+};
+export const setHeaderPositionVariable = (position: string) => {
+    document.documentElement.style.setProperty(HEADER_POSITION_VAR, position);
+};
 export const useStickyHeaderHeight = () => {
     useEffect(() => {
         const elementParams = getElementSize('.js-header');
@@ -10,13 +21,15 @@ export const useStickyHeaderHeight = () => {
         const resizeHandler = () => {
             const elementParams = getElementSize('.js-header');
             if (elementParams && page) {
-                page.style.paddingTop = `${elementParams?.height.toFixed()}px`;
+                setHeaderHeightVariable(elementParams.clientRect.height ?? 0);
+                setHeaderPositionVariable('absolute');
             }
         };
 
         if (elementParams && page) {
             window.addEventListener('resize', resizeHandler);
-            page.style.paddingTop = `${elementParams?.height.toFixed()}px`;
+            setHeaderHeightVariable(elementParams.clientRect.height ?? 0);
+            setHeaderPositionVariable('absolute');
         }
 
         return () => window.removeEventListener('resize', resizeHandler);
--- src/widgets/Header/ui/Header/HeaderClient/index.ts
+++ src/widgets/Header/ui/Header/HeaderClient/index.ts
@@ -1,2 +1,1 @@
-export { Header } from './Header';
 export { HeaderClient } from './HeaderClient';
